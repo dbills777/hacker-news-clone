b087534579dcf85d630b2a18c2b736e84846d560
Derrick Bills <dbills777@gmail.com>
2021-03-05 08:15:55 -0700

    extract only hostname from url

--- src/App.svelte
+++ src/App.svelte
@@ -28,20 +28,20 @@
 	}
 </style>
 	<script>
-		let topThirty = []
+		let getThirtyArticles = []
 	async function getListOfStoryID() {
 		const res = await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`);
 		const text = await res.json();
 		if (res.ok) {
-			topThirty = text
-			topThirty.length = 30
-			console.log(topThirty)
-			return topThirty;
+			getThirtyArticles = text
+			getThirtyArticles.length = 30
+			console.log(getThirtyArticles)
+			return getThirtyArticles;
 		} else {
 			throw new Error(text);
 		}
 	}
-	let promise = getListOfStoryID();
+	let arraOfThirty = getListOfStoryID();
 
 		let StoryArray = []
 	async function getSingleStory(storyID) {
@@ -65,17 +65,29 @@
 	let allStories= []
 	let getMoreStories = fetchlist.then(item=>{
 			const mapItems = item.map(async item=>{
-				console.log(item)
+				// console.log(item)
 				const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${item}.json?print=pretty`)
 				const data = await response.json()
+				// console.log(data)
 				return data
 			})
-			console.log(mapItems)
+			// console.log(mapItems)
+
 			return mapItems
 		})
 	getMoreStories.then(story =>{
 		Promise.all(story).then(story => {
-			allStories = story
+			const testChange = [...story]
+			let result = testChange.map(story =>({
+				author: story.by,
+				title: story.title,
+				url:  new URL(`${story.url}`)
+
+			}))
+			console.log(result)
+			allStories = result
+
+
 		})
 	})
 
@@ -83,27 +95,24 @@
 
 <main>
 
+	{#each allStories as story, i}
+		<p>{i + 1}. <span class="title"> {story.author} </span> <span><a class = 'url'href="{story.url}">
+			{story.url.origin}
+			</a> </span> </p>
+	{/each}
 
-{#each allStories as story, i}
-	<p>{i + 1}. <span class="title"> {story.title} </span> <span><a class = 'url'href="{story.url}">{story.url}</a> </span> </p>
-{/each}
 
-{#await SingleStory}
-	<p>...waiting</p>
-{:then SingleStory}
-	<p> 1. {SingleStory.title}, {SingleStory.url}</p>
-	<p>{SingleStory.score} points by {SingleStory.by} time as number{SingleStory.time} | {SingleStory.descendants} comments </p>
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
+<!--
+	{#await SingleStory}
+		<p>...waiting</p>
+	{:then SingleStory}
+		<p> 1. {SingleStory.title}, {SingleStory.url}</p>
+		<p>{SingleStory.score} points by {SingleStory.by} time as number{SingleStory.time} | {SingleStory.descendants} comments </p>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+-->
 
-{#await fetchlist}
-	<p>...waiting</p>
-{:then fetchlist}
-	<p>The number is {fetchlist}</p>
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
 
 
 
